c17703b5005f6927d1bab8664160dd66889c69ee
KSenior2608 <ashishkumaras789@gmail.com>
2021-07-06 22:06:21 +0530

    [ADD] authenticate middleware for posts route

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+const { json } = require("express");
 const express = require("express");
 const jwt = require("jsonwebtoken");
 const app =express();
@@ -17,9 +18,34 @@ var posts = [
     }
 ]
 
-app.get("/posts",function(req,res){
-    res.send(posts.json());
+app.get("/posts",authenticate,function(req,res){
+    let user = req.user;
+    // console.log(user);
+    let userPosts =[];
+    posts.forEach(function(post){
+        if(post.author == user.username){
+            userPosts.push(post);
+        }
+    }) 
+    // console.log(userPosts);
+    res.json(userPosts);
 });
+
+function authenticate(req,res,next){
+    let authHeader = req.headers["authorization"];
+    let token=authHeader && authHeader.split(" ")[1];
+    if(!token){
+        res.sendStatus(401);
+    }
+    jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,function(err,user){
+        if(err){
+            res.sendStatus(403);
+        }else{
+            req.user = user;
+            next();
+        }
+    });
+}
 
 app.post("/login",function(req,res){
     //authenticate
